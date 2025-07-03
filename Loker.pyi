58fbb359858546c3df302cb5dcd861b297a82658
ahmadsopiyan27 <ahmmadsopiyann@gmail.com>
2025-07-03 20:48:49 +0700

    update scraping

--- Loker.pyi
+++ Loker.pyi
@@ -8,8 +8,8 @@ import requests
 
 # base_url = f'https://id.jobstreet.com/id/qa-automation-jobs/in-jakarta'
 
-# response = requests.get(base_url)
-# soup = BeautifulSoup(response.text, 'html.parser')
+response = requests.get(base_url)
+soup = BeautifulSoup(response.text, 'html.parser')
 
 # print(soup)
 
